b6bab8b2e7948bb4b8c6d051e0e99e2ac1051b78
Unknown <clayvass@yahoo.com>
2018-06-25 04:25:22 -0500

    Giant Commit

---
+++ Assets/SimpleMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class SimpleMovement : MonoBehaviour {
+    public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed; //* Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
+    }
+}
---
+++ Assets/TouchTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(Input.touchCount);
+        if (Input.touchCount > 0)
+        {
+            Input.GetTouch(0);
+            Application.Quit();
+        }
+
+	}
+}
