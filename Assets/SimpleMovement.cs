cddbbd2baea70e1b5a1d33544c10ca457845b0d3
Unknown <clayvass@yahoo.com>
2018-07-01 18:06:21 -0500

    Forward Movement ONLY

--- Assets/SimpleMovement.cs
+++ Assets/SimpleMovement.cs
@@ -9,14 +9,24 @@ public class SimpleMovement : MonoBehaviour {
     void Start () {
 		
 	}
-	
-	// Update is called once per frame
-    void Update()
+    private void Update()
     {
         CharacterController controller = GetComponent<CharacterController>();
         transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         float curSpeed = speed; //* Input.GetAxis("Vertical");
         controller.SimpleMove(forward * curSpeed);
+    }
+
+    public void MoveForward()
+    {
+        speed = 0.5F;
+        rotateSpeed = 3.0F;
+    }
+
+    public void StopMovement()
+    {
+        speed = 0.0F;
+        rotateSpeed = 0.0F;
     }
 }
